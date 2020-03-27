a2c4afe30b35ddcf21c64e095c18d7038a6ffd99
Peter Gray <hilow2341@gmail.com>
2020-03-26 20:47:46 -0400

    conflict resolved

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 
 //FIND MEAL
@@ -47,39 +46,3 @@ $("#find").on("click", function () {
 });
 
 
-=======
-
-
-//FIND MEAL
-
-$("#find").on("click", function () {
-	var ingredients = [];
-	$(".input").each(function () {
-		ingredients.push($(this).val());
-	});
-
-	var settings = {
-		"async": true,
-		"crossDomain": true,
-		"url": "https://webknox-recipes.p.rapidapi.com/recipes/findByIngredients?number=5&ingredients=" + ingredients.join(","),
-		"method": "GET",
-		"headers": {
-			"x-rapidapi-host": "webknox-recipes.p.rapidapi.com",
-			"x-rapidapi-key": "5055de4237mshb8184148441534fp1f5c84jsnf1aa75b94cd0"
-		},
-
-	}
-	$.ajax(settings).then(function (response) {
-		console.log(response);
-		response.forEach(response => {
-			var recipeDiv = $("<div>")
-			recipeDiv.text(response.title)
-			$("#recipes").append(recipeDiv)
-			recipeDiv.hover(function (event) {
-				$("#recipe-image").attr("src", response.image)
-			})
-		});
-	});
-
-});
->>>>>>> master
